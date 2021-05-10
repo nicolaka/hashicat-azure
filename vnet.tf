83bb3b855164eac0048a8254e7a5fb284a6cc363
Nico Kabar <nicolaka@gmail.com>
2021-05-10 17:33:45 +0000

    Added vnet module

---
+++ vnet.tf
@@ -0,0 +1,7 @@
+# vnet.tf
+module "network" {
+  source  = "app.terraform.io/nico-tfc/network/azurerm"
+  version = "3.0.1"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
+  # insert required variables here
+}
